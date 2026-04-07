b78ff7fcdb6c8f242efcbc7664488132f1c28661
irfan <116457057+fann22@users.noreply.github.com>
2026-04-08 02:18:26 +0700

    Add new repository and update package requirements

--- xmake.lua
+++ xmake.lua
@@ -1,18 +1,9 @@
 add_rules("mode.debug", "mode.release")
 
 add_repositories("levimc-repo https://github.com/LiteLDev/xmake-repo.git")
+add_repositories("miracleforest-repo https://github.com/MiracleForest/xmake-repo.git")
 
-option("target_type")
-    set_default("server")
-    set_showmenu(true)
-    set_values("server")
-option_end()
-
--- add_requires("levilamina x.x.x") for a specific version
--- add_requires("levilamina develop") to use develop version
--- please note that you should add bdslibrary yourself if using dev version
-add_requires("levilamina", {configs = {target_type = get_config("target_type")}})
-
+add_requires("levilamina", {configs = {target_type = "server" }})
 add_requires("levibuildscript")
 add_requires("ilistenattentively")
 
@@ -25,8 +16,7 @@ target("unbreakable-farmland") -- Change this to your mod name.
     add_rules("@levibuildscript/modpacker")
     add_cxflags( "/EHa", "/utf-8", "/W4", "/w44265", "/w44289", "/w44296", "/w45263", "/w44738", "/w45204")
     add_defines("NOMINMAX", "UNICODE")
-    add_packages("levilamina")
-    add_packages("ilistenattentively")
+    add_packages("levilamina", "ilistenattentively")
     set_exceptions("none") -- To avoid conflicts with /EHa.
     set_kind("shared")
     set_languages("c++20")
